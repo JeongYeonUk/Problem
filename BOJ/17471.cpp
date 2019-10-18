8d21b39725ca70a7338f0d78b746cd2d30977322
연욱 정 <vire3064@gmail.com>
2019-10-18 19:00:32 +0900

    191018_1900

--- BOJ/17471.cpp
+++ BOJ/17471.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#pragma warning(disable:4996)
+#include <cstdio>
 #include <cstring>
 #include <cmath>
 #include <vector>
@@ -12,110 +13,113 @@ const int INF = 987654321;
 
 int person[10];
 vector<int> adj[10];
-bool checka[10], checkb[10];
+int pick[10];
+int n, ret;
+vector<int> a, b;
 bool visita[10], visitb[10];
-
-void goa(int idx)
+void aConti(int x)
 {
-  if (visita[idx])
-    return;
-  visita[idx] = true;
-  for (int i = 0; i < adj[idx].size(); ++i)
-  {
-    int y = adj[idx][i];
-    if (checka[y] && !visita[y])
-      goa(y);
-  }
-  return;
+	if (visita[x])
+		return;
+	visita[x] = true;
+	for (int i = 0; i < (int)adj[x].size(); ++i)
+	{
+		if (!visita[adj[x][i]] && pick[adj[x][i]])
+			aConti(adj[x][i]);
+	}
 }
 
-void gob(int idx)
+void bConti(int x)
 {
-  if (visitb[idx])
-    return;
-  visitb[idx] = true;
-  for (int i = 0; i < adj[idx].size(); ++i)
-  {
-    int y = adj[idx][i];
-    if (checkb[y] && !visitb[y])
-      gob(y);
-  }
-  return;
+	if (visitb[x])
+		return;
+	visitb[x] = true;
+	for (int i = 0; i < (int)adj[x].size(); ++i)
+	{
+		if (!visitb[adj[x][i]] && !pick[adj[x][i]])
+			bConti(adj[x][i]);
+	}
+}
+
+void dfs(int cur, int pick_count, int goal)
+{
+	if (pick_count == goal)
+	{
+		int A = 0, B = 0;
+		a.clear(); b.clear();
+		memset(visita, false, sizeof(visita));
+		memset(visitb, false, sizeof(visitb));
+		for (int i = 0; i < n; ++i)
+		{
+			if (pick[i] == 1)
+			{
+				a.push_back(i);
+				A += person[i];
+			}
+			else
+			{
+				b.push_back(i);
+				B += person[i];
+			}
+		}
+		aConti(a[0]);
+		bool ok = true;
+		for (int i = 0; i < (int)a.size(); ++i)
+		{
+			if (!visita[a[i]])
+			{
+				ok = false;
+				break;
+			}
+		}
+		if (!ok) return;
+		bConti(b[0]);
+		for (int i = 0; i < (int)b.size(); ++i)
+		{
+			if (!visitb[b[i]])
+			{
+				ok = false;
+				break;
+			}
+		}
+		if (!ok) return;
+
+		if (ret > abs(A - B))
+			ret = abs(A - B);		
+		return;
+	}
+	for (int i = cur; i < n; ++i)
+	{
+		pick[i] = 1;
+		dfs(cur + 1, pick_count + 1, goal);
+		pick[i] = 0;
+	}
 }
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr); cout.tie(nullptr);
+	scanf("%d", &n);
+	for (int i = 0; i < n; ++i)
+	{
+		scanf("%d", &person[i]);
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		int cnt; scanf("%d", &cnt);
+		for (int j = 0; j < cnt; ++j)
+		{
+			int x;
+			scanf("%d", &x);
+			adj[i].push_back(x - 1);
+		}
+	}
+	ret = INF;
+	for (int i = 1; i <= n/2; ++i)
+	{
 
-  int n; cin >> n;
-  for (int i = 0; i < n; ++i)
-    cin >> person[i];
-
-  for (int i = 0; i < n; ++i)
-  {
-    int num; cin >> num;
-    for (int j = 0; j < num; ++j)
-    {
-      int x; cin >> x;
-      adj[i].push_back(x - 1);
-    }
-  }
-
-  int ans = -1;
-  for (int i = 1; i < (1 << n) - 1; ++i)
-  {
-    vector<int> a, b;
-    memset(checka, false, sizeof(checka));
-    memset(checkb, false, sizeof(checkb));
-    memset(visita, false, sizeof(visita));
-    memset(visitb, false, sizeof(visitb));
-    int suma = 0, sumb = 0;
-    for (int j = 0; j < n; ++j)
-    {
-      if (i & (1 << j))
-      {
-        a.push_back(j);
-        suma += person[j];
-        checka[j] = true;
-      }
-      else
-      {
-        b.push_back(j);
-        sumb += person[j];
-        checkb[j] = true;
-      }
-    }
-
-    goa(a[0]);
-    bool ok = true;
-    for (int j = 0; j < a.size(); ++j)
-    {
-      if (!visita[a[j]])
-      {
-        ok = false;
-        break;
-      }
-    }
-    if (!ok)
-      continue;
-
-    gob(b[0]);
-    for (int j = 0; j < b.size(); ++j)
-    {
-      if (!visitb[b[j]])
-      {
-        ok = false;
-        break;
-      }
-    }
-    if (!ok)
-      continue;
-
-    int diff = abs(suma - sumb);
-    if (ans == -1 || ans > diff)
-      ans = diff;
-  }
-  cout << ans << endl;
-  return 0;
+		dfs(0,0, i);
+	}
+	if (ret == INF) ret = -1;
+	printf("%d\n", ret);
+	return 0;
 }
