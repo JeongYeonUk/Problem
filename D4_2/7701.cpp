d701706bb48792584a4fbb880bfc3b3a352392fe
JeongYeonUk <vire3064@gmail.com>
2020-03-12 14:20:27 +0900

    2020-03-12 ** 출력만 하는 것이면 ** ** 굳이 안지워도 된다 **

--- D4_2/7701.cpp
+++ D4_2/7701.cpp
@@ -1,52 +1,48 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstring>
-#include <string>
-#include <vector>
-#include <algorithm>
 using namespace std;
-
-typedef long long ll;
 
 #define endl '\n'
 
+typedef long long ll;
+
 const int INF = 987654321;
-
-vector<string> s;
-string str;
 int N;
+string arr[20001];
 
-bool cmp(const string& a, const string& b) {
-	if (a.length() == b.length()) {
-		return a < b;
-	}
-	else {
-		return a.length() < b.length();
-	}
-}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-int main() {
+    int tc = 0;
 
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    cin >> tc;
+    cin >> N;
 
-	freopen("input.txt", "r", stdin);
 
-	int tc; cin >> tc;
-	for (int t = 1; t <= tc; ++t) {
-		s.clear();
-		cin >> N;
-		for (int i = 0; i < N; ++i) {
-			cin >> str;
-			s.push_back(str);
-		}
-		sort(s.begin(), s.end(), cmp);
-		s.erase(unique(s.begin(), s.end()), s.end());
-		cout << "#" << t << endl;
-		for (int i = 0; i < s.size(); ++i) {
-			cout << s[i] << endl;
-		}
-	}
-
-	return 0;
+    for (int t = 1; t <= tc; t++) {
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                if (arr[i].size() > arr[j].size()) {
+                    swap(arr[i], arr[j]);
+                }
+                else if (arr[i].size() == arr[j].size())
+                {
+                    if (arr[i] > arr[j])
+                        swap(arr[i], arr[j]);
+                }
+            }
+        }
+        cout << "#" << t << endl;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == arr[i + 1])
+                continue;
+            cout << arr[i] << endl;
+        }
+    }
+    return 0;
 }
