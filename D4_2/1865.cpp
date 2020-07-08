91565ce5d8eebcd2bc6787499a64eb7d0a79a8ee
JeongYeonUk <vire3064@gmail.com>
2020-07-09 00:07:45 +0900

    2020-07-09  00:07
    1) SW EXPERT D4 1865
        - not solve

---
+++ D4_2/1865.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+#define endl '\n'
+#define X first
+#define Y second
+#define INF 987654321
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cout.tie(NULL); cin.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+
+	return 0;
+}
