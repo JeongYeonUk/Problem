0dbce9c3da393aa628029ab359887ea1da4b9750
JeongYeonUk <vire3064@gmail.com>
2020-07-12 00:35:16 +0900

    2020-07-12  00:34
    1) SW EXPERT D4  1865 - DP
    2) SW EXPERT D4  9282 - DP

--- D4_2/1865.cpp
+++ D4_2/1865.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
+#include <cstdio>
 #include <cstring>
 #include <algorithm>
 using namespace std;
@@ -13,13 +13,46 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+const int maxN = 16 + 1;
+
+double p[maxN][maxN];
+double dp[maxN][1 << maxN];
+int n;
+
+double solve(int y, int visit)
+{
+	double& ret = dp[y][visit];
+	if (ret > 0) return ret;
+
+	if (y >= n) return 1;
+	ret = 0;
+	for (int i = 0; i < n; ++i) {
+		if (visit & (1 << i)) continue;
+		if (p[y][i] == 0) continue;
+		ret = max(ret, solve(y + 1, (visit | 1 << i)) * p[y][i]);
+	}
+	return ret;
+}
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cout.tie(NULL); cin.tie(NULL);
-
 	freopen("input.txt", "r", stdin);
+
+	int T; scanf("%d", &T);
+	for (int t = 1; t <= T; ++t) {
+		scanf("%d", &n);
+		memset(dp, 0, sizeof(dp));
+
+		double input;
+		for (int y = 0; y < n; ++y) {
+			for (int x = 0; x < n; ++x) {
+				scanf("%lf", &input);
+				p[y][x] = input / 100;
+			}
+		}
+
+		printf("#%d %.6lf\n", t, solve(0, 0) * 100);
+	}
 
 	return 0;
 }
--- D4_2/9282.cpp
+++ D4_2/9282.cpp
@@ -13,6 +13,34 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
+const int maxN = 50 + 1;
+
+int n, m;
+int choco[maxN][maxN];
+int sum[maxN][maxN];
+int dp[maxN][maxN][maxN][maxN];
+
+int solve(int sy, int sx, int ey, int ex)
+{
+	if (ey == sy && ex == sx) return 0;
+
+	int& ret = dp[sy][sx][ey][ex];
+	if (ret != -1) return ret;
+
+	int temp = sum[ey][ex] - sum[sy - 1][ex] - sum[ey][sx - 1] + sum[sy - 1][sx - 1];
+
+	int imin = INF;
+
+	for (int x = sx; x < ex; ++x) {
+		imin = min(imin, temp + (solve(sy, sx, ey, x) + solve(sy, x + 1, ey, ex)));
+	}
+
+	for (int y = sy; y < ey; ++y) {
+		imin = min(imin, temp + (solve(sy, sx, y, ex) + solve(y + 1, sx, ey, ex)));
+	}
+
+	return ret = imin;
+}
 
 int main()
 {
@@ -20,6 +48,28 @@ int main()
 	cout.tie(NULL); cin.tie(NULL);
 
 	freopen("input.txt", "r", stdin);
+
+	int T; cin >> T;
+	for (int t = 1; t <= T; ++t) {
+		cin >> n >> m;
+		for (int y = 1; y <= n; ++y) {
+			for (int x = 1; x <= m; ++x) {
+				cin >> choco[y][x];
+			}
+		}
+
+		memset(dp, -1, sizeof(dp));
+
+		// 누적 합을 미리 구해 놓는다.
+		for (int y = 1; y <= n; ++y) {
+			for (int x = 1; x <= m; ++x) {
+				sum[y][x] = sum[y - 1][x] + sum[y][x - 1] - sum[y - 1][x - 1] + choco[y][x];
+			}
+		}
+
+		int ans = solve(1, 1, n, m);
+		cout << '#' << t << ' ' << ans << endl;
+	}
 
 	return 0;
 }
